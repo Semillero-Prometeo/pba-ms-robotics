0b618895be723e578d15bac0fb3e10dddbb09ddc
Santiago Arias <santiari05@hotmail.com>
2026-03-28 18:28:32 -0500

    Remove commented-out import statement in nats_subscribers.py for cleaner code.

--- src/core/nats/nats_subscribers.py
+++ src/core/nats/nats_subscribers.py
@@ -4,9 +4,6 @@ from nats.aio.client import Client as NATSClient
 from src.app_controller import AppController
 from src.core.settings.env import ROBOTICS_MS
 
-# Load controllers
-# from src.app.app_controller import AppController
-
 
 def create_subscribers(nats_client: NATSClient) -> list[dict[str, Any]]:
     return [
